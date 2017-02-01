235122b214b99c980441efbd9d55d6348a781456
fbell123 <fbell.pro@gmail.com>
2017-02-01 12:12:51 +0000

    encapusulate state method for test

--- hwr/src/pages/Detail.js
+++ hwr/src/pages/Detail.js
@@ -21,11 +21,15 @@ class Detail extends React.Component {
     const baseURL = 'https://api.github.com/repos/facebook';
     ajax.get(`${baseURL}/${this.props.params.repo}/${type}`).end((error, response) => {
       if (!error && response) {
-        this.setState({ [type]: response.body });
+        this.saveFeed(type, response.body);
       } else {
         console.log(`Error fetching ${type}`, error);
       }
     });
+  }
+
+  saveFeed(type, contents) {
+    this.setState({ [type]: response.body });
   }
 
   componentWillMount() {
@@ -38,7 +42,7 @@ class Detail extends React.Component {
     return this.state.commits.map((commit, index) => {
       const author = commit.author ? commit.author.login : 'Anonymous';
 
-      return (<p key={index}>
+      return (<p key={index} className="github">
         <Link to={`user/${author}`}>{author}</Link>:
         <a href={commit.html_url}> {commit.commit.message}</a>.
       </p>);
@@ -49,7 +53,7 @@ class Detail extends React.Component {
     return this.state.forks.map((fork, index) => {
       const owner = fork.owner ? fork.owner.login : 'Anonymous';
 
-      return (<p key={index}>
+      return (<p key={index} className="github">
         <Link to={`user/${owner}`}>{owner}</Link> forked to
         <a href={fork.html_url}> {fork.html_url}</a> at {fork.created_at}.
       </p>
@@ -61,7 +65,7 @@ class Detail extends React.Component {
     return this.state.pulls.map((pull, index) => {
       const user = pull.user ? pull.user.login : 'Anonymous';
 
-      return (<p key={index}>
+      return (<p key={index} className="github">
         <Link to={`user/${user}`}>{user}</Link>:
         <a href={pull.html_url}> {pull.body}</a>
       </p>)
